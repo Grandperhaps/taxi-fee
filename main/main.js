0a7393d732f64a0fd2a9d18fa543966cc2a12a34
Grandperhaps <45555670+Grandperhaps@users.noreply.github.com>
2019-03-23 16:48:27 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
-};
+   const {distance,parkTime}=inputs
+    let price=0
+    if(distance<=2)
+    {price=(6+time(parkTime));}
+    else
+        if(distance<=8)
+    {price=(6+(distance-2)*0.8+time(parkTime));}
+    else
+    {price=(6+6*0.8+(distance-8)*1.2+time(parkTime));}
+    return Math.round(price);
+}
+function time(parkTime)
+{
+   return parkTime*0.25
+}
